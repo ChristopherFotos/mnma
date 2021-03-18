31595fdd94862e883f57c79a9830f63ecbe3e06b
Chris Johns <chrisfotosmusic@gmail.com>
2021-03-18 12:11:56 -0400

    fix mobile menu issue

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import "byob-css";
 
-// let navs = Array.from(document.getElementsByTagName('nav'))
-// navs.forEach(n => {
-//     let children = Array.from(n.childNodes)
-//     children.forEach(c => {
-//         if(c.nodeName === 'A'){
-//             c.tabIndex = '0'
-//         }
-//     })
-// })
+
+console.log('running')
+
+let navs = Array.from(document.getElementsByTagName('nav'))
+navs.forEach(n => {
+    let children = Array.from(n.childNodes)
+    children.forEach(c => {
+        if(c.nodeName === 'A'){
+            c.tabIndex = '0'
+        }
+    })
+})
